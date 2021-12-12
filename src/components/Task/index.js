1c1efbacef5ee2847dba637c708a52de075d2ab7
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2021-12-12 20:24:50 +0700

    Add TaskList

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+export default function Task({task: {id, title, state}, onArchiveTask, onPinTask}) {
   return (
-<div className={`list-item ${state}`}>
+    <div className={`list-item ${state}`}>
       <label className="checkbox">
         <input
           type="checkbox"
@@ -19,14 +19,14 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input type="text" value={title} readOnly={true} placeholder="Input title"/>
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
         {state !== 'TASK_ARCHIVED' && (
           // eslint-disable-next-line jsx-a11y/anchor-is-valid
           <a onClick={() => onPinTask(id)}>
-            <span className={`icon-star`} id={`pinTask-${id}`} aria-label={`pinTask-${id}`} />
+            <span className={`icon-star`} id={`pinTask-${id}`} aria-label={`pinTask-${id}`}/>
           </a>
         )}
       </div>
